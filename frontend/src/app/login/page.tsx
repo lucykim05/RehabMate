dffe4dd3b04d1b8364360a0e68d9180779bdb845
lucy-kim05 <effelt0522@naver.com>
2025-09-08 17:06:21 +0900

    feat: create loginpage

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useState } from 'react';
-import api from '@/libs/api';
+import { login } from '@/libs/api';
 import FormInput from '@/components/FormInput';
 
 interface LoginResponse {
@@ -16,36 +16,64 @@ export default function LoginPage() {
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      const res = await api.post<LoginResponse>('/auth/login', null, {
-        params: { email, password },
-      });
-
-      // 이제 res.data는 LoginResponse 타입으로 인식됨
-      localStorage.setItem('accessToken', res.data.accessToken);
-      alert(`로그인 성공! 환영합니다 ${res.data.email}`);
+      const res = await login(email, password);
+      localStorage.setItem('accessToken', res.accessToken);
+      alert(`로그인 성공! 환영합니다 ${res.email}`);
     } catch (err) {
       setError('로그인 실패');
     }
   };
 
   return (
-    <form onSubmit={handleLogin}>
-      <FormInput
-        label="이메일"
-        type="email"
-        value={email}
-        placeholder="이메일 입력"
-        onChange={setEmail}
-      />
-      <FormInput
-        label="비밀번호"
-        type="password"
-        value={password}
-        placeholder="비밀번호 입력"
-        onChange={setPassword}
-      />
-      <button type="submit">로그인</button>
-      {error && <p>{error}</p>}
-    </form>
+    <main className="min-h-screen flex items-center justify-center bg-gray-50">
+      <div className="w-full max-w-md bg-white shadow-lg rounded-2xl p-8">
+        {/* 제목 */}
+        <h1 className="text-2xl text-gray-800 font-bold text-center mb-6">
+          로그인
+        </h1>
+
+        {/* 폼 */}
+        <form onSubmit={handleLogin} className="space-y-4 text-gray-500">
+          <FormInput
+            label="이메일"
+            type="email"
+            value={email}
+            placeholder="이메일 입력"
+            onChange={setEmail}
+          />
+          <FormInput
+            label="비밀번호"
+            type="password"
+            value={password}
+            placeholder="비밀번호 입력"
+            onChange={setPassword}
+          />
+
+          {/* 에러 메시지 */}
+          {error && (
+            <p className="text-red-500 text-sm mt-2 text-center">{error}</p>
+          )}
+
+          {/* 버튼 */}
+          <button
+            type="submit"
+            className="w-full py-2 bg-blue-500 hover:bg-blue-600 cursor-pointer text-white rounded-lg font-medium transition"
+          >
+            로그인
+          </button>
+        </form>
+
+        {/* 회원가입 안내 */}
+        <p className="text-center text-sm text-gray-500 mt-6">
+          아직 계정이 없으신가요?{' '}
+          <a
+            href="/register"
+            className="text-blue-500 hover:underline font-medium"
+          >
+            회원가입
+          </a>
+        </p>
+      </div>
+    </main>
   );
 }
